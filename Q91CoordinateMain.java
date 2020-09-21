dd7a7c1a545f458a951cd78fd3c6c42832c4a16d
mjp002 <67648603+mjp002@users.noreply.github.com>
2020-09-21 17:59:45 +0900

    Create Q91CoordinateMain.java

---
+++ Q91CoordinateMain.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner stdIn = new Scanner(System.in);
+
+    System.out.println("좌표 p를 입력하세요.");
+    System.out.print("X좌표 : ");double x = stdIn.nextDouble();
+    System.out.print("y좌표 : ");double y = stdIn.nextDouble();
+
+    Coordinate p = new Coordinate(x,y);
+    System.out.println("p = (" + p.getX() + ", "+p.getY()+ ")");
+
+    Coordinate q = p;
+    q.set(9.9,9.9);
+
+    System.out.println("p=(" + p.getX() + ", " + p.getY()+")");
+    System.out.println("q=(" + q.getX() + ", " + q.getY()+")");
+  }
+}
