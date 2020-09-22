86771772c78a148de8a01048ef7b97ec5d685d63
mjp002 <67648603+mjp002@users.noreply.github.com>
2020-09-22 10:13:57 +0900

    Create Q102IdClass.java

---
+++ Q102IdClass.java
@@ -0,0 +1,36 @@
+<Main.java>
+
+class Main {
+  public static void main(String[] args) {
+    Id a = new Id();
+    Id b = new Id();
+
+    System.out.println("a의 식별 번호 : " + a.getId());
+    System.out.println("b의 식별 번호 : " + b.getId());
+
+    System.out.println("Id.counter : " + Id.counter);
+    System.out.println("a.counter : " + a.counter);
+    System.out.println("b.counter : " + b.counter);
+  }
+}
+
+
+<Id.java>
+
+class Id {
+  static int counter = 0;
+  private int id; // private 설정하면 외부 접근 불가
+
+
+  public Id() {
+    id = ++counter;
+
+
+  }
+
+  public int getId() {
+    return id;
+  }
+
+
+}
