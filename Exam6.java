397af724b7c8727cfde4e135efa890e7e927b737
mjp002 <67648603+mjp002@users.noreply.github.com>
2020-09-25 17:15:36 +0900

    Create Exam6.java

---
+++ Exam6.java
@@ -0,0 +1,13 @@
+class Main {
+  public static void main(String[] args) {
+    final int a = 3;
+    final int b = 5;
+    int x = 1;
+
+    x=(a+b)/2;
+    
+    System.out.println("a = "+ a);
+    System.out.println("b = "+ b);
+    System.out.println("x = "+ x);
+  }
+}
