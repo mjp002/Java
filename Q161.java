0d3191da145d6c205939ec4728c647798c50d931
mjp002 <67648603+mjp002@users.noreply.github.com>
2020-09-25 17:11:41 +0900

    Create Q161.java

---
+++ Q161.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("x값 : ");int a = sc.nextInt();
+        System.out.print("y값 : ");int b = sc.nextInt();
+        System.out.println("x*y="+a*b);
+        System.out.println("x/y="+a/b);
+    }
+}
